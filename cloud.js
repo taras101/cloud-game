e8aaaac6495c1a56fe51ccd5ab34f8237ed9c919
taras101 <taras101@gmail.com>
2015-10-20 12:14:53 -0400

    adding sound

--- cloud.js
+++ cloud.js
@@ -1,8 +1,8 @@
-var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
-
+var game = new Phaser.Game(1280, 480, Phaser.CANVAS, 'game');
+var text;
 var PhaserGame = function () {
     this.bg = null;
-    this.trees = null;
+    this.mountain = null;
 
     this.player = null;
 
@@ -37,46 +37,56 @@ PhaserGame.prototype = {
 
         //  We need this because the assets are on Amazon S3
         //  Remove the next 2 lines if running locally
-        this.load.image('diamond', 'assets/diamond.png');
-        this.load.image('trees', 'assets/trees-h.png');
-        this.load.image('background', 'assets/clouds-h.png');
-        this.load.image('platform', 'assets/platform.png');
-        this.load.image('cloud-platform', 'assets/cloud-platform.png');
-        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        this.load.image('pizza', 'assets/pizza1.svg');
+        this.load.image('mountain', 'assets/mountain3.svg');
+        this.load.image('mountain1', 'assets/mountain2.svg');
+        this.load.image('background', 'assets/sky1.svg');
+        this.load.image('platform', 'assets/flat2.svg');
+        this.load.image('float', 'assets/float.svg');
+        this.load.image('float1', 'assets/float1.svg');
+        this.load.spritesheet('egg', 'assets/egg.png', 32, 48);
 
-        //  Note: Graphics are Copyright 2015 Photon Storm Ltd.
         this.load.audio('burp', 'assets/burp.m4a');
+        this.load.audio('jump', 'assets/jump.wav');
+        this.load.audio('goal', 'assets/goal.wav');
+        this.load.audio('death', 'assets/death.wav');
 
     },
 
     create: function () {
 
-        this.background = this.add.tileSprite(0, 0, 640, 480, 'background');
-        this.background.fixedToCamera = true;
+    text = game.add.text(game.world.centerX, game.world.centerY, "- phaser -\nclick to remove", { font: "65px Arial", fill: "#ff0044", align: "center" });
+    text.anchor.setTo(0.5, 0.5);
 
-        this.trees = this.add.tileSprite(0, 364, 640, 116, 'trees');
-        this.trees.fixedToCamera = true;
-        // Diamond
-        this.diamond = this.add.physicsGroup();
-        this.diamond.create(2460, 30, 'diamond');
-        this.diamond.setAll('body.allowGravity', false);
-        this.diamond.setAll('body.immovable', true);
+    game.input.onDown.addOnce(removeText, this);
+        this.background = this.add.tileSprite(0, 0, 1280, 480, 'background');
+        this.background.fixedToCamera = true;
+        this.mountain1 = this.add.tileSprite(0, 364, 1280, 116, 'mountain1');
+        this.mountain1.fixedToCamera = true;
+        this.mountain = this.add.tileSprite(0, 364, 1280, 116, 'mountain');
+        this.mountain.fixedToCamera = true;
+        
+        // pizza
+        this.pizza = this.add.physicsGroup();
+        this.pizza.create(2460, 30, 'pizza');
+        this.pizza.setAll('body.allowGravity', false);
+        this.pizza.setAll('body.immovable', true);
         emitter = game.add.emitter(2460, 30, 500);
 
-        emitter.makeParticles('diamond');
+        emitter.makeParticles('pizza');
         emitter.gravity = 200;
 
         //  Platforms that don't move
         this.stationary = this.add.physicsGroup();
 
         this.stationary.create(40, 396, 'platform');
-        this.stationary.create(632, 120, 'platform');
+        this.stationary.create(600, 120, 'platform');
         this.stationary.create(1100, 330, 'platform');
         this.stationary.create(1540, 250, 'platform');
-        this.stationary.create(1900, 150, 'platform');
+        this.stationary.create(1900, 140, 'platform');
         this.stationary.create(1900, 350, 'platform');
-        this.stationary.create(2220, 200, 'platform');
-        this.stationary.create(2400, 124, 'platform');
+        this.stationary.create(2208, 216, 'platform');
+        this.stationary.create(2380, 144, 'platform');
 
         this.stationary.setAll('body.allowGravity', false);
         this.stationary.setAll('body.immovable', true);
@@ -86,31 +96,31 @@ PhaserGame.prototype = {
         //  Platforms that move
         this.clouds = this.add.physicsGroup();
 
-        var cloud1 = new CloudPlatform(this.game, 300, 450, 'cloud-platform', this.clouds);
+        var cloud1 = new CloudPlatform(this.game, 300, 450, 'float1', this.clouds);
 
         cloud1.addMotionPath([
-            { x: "+200", xSpeed: 1000, xEase: "Linear", y: "-200", ySpeed: 1000, yEase: "Sine.easeIn" },
-            { x: "-200", xSpeed: 1000, xEase: "Linear", y: "-200", ySpeed: 1000, yEase: "Sine.easeOut" },
+            { x: "+200", xSpeed: 2000, xEase: "Linear", y: "-200", ySpeed: 2000, yEase: "Sine.easeIn" },
+            { x: "-200", xSpeed: 2000, xEase: "Linear", y: "-200", ySpeed: 2000, yEase: "Linear" },
             { x: "-200", xSpeed: 2000, xEase: "Linear", y: "+200", ySpeed: 2000, yEase: "Sine.easeIn" },
-            { x: "+200", xSpeed: 1000, xEase: "Linear", y: "+200", ySpeed: 1000, yEase: "Sine.easeOut" }
+            { x: "+200", xSpeed: 2000, xEase: "Linear", y: "+200", ySpeed: 2000, yEase: "Sine.easeOut" }
         ]);
 
-        var cloud2 = new CloudPlatform(this.game, 800, 96, 'cloud-platform', this.clouds);
+        var cloud2 = new CloudPlatform(this.game, 800, 96, 'float1', this.clouds);
 
         cloud2.addMotionPath([
             { x: "+0", xSpeed: 2000, xEase: "Linear", y: "+300", ySpeed: 2000, yEase: "Sine.easeIn" },
-            { x: "-0", xSpeed: 500, xEase: "Linear", y: "-300", ySpeed: 500, yEase: "Linear" }
+            { x: "-0", xSpeed: 1000, xEase: "Linear", y: "-300", ySpeed: 1000, yEase: "Linear" }
         ]);
 
-        var cloud3 = new CloudPlatform(this.game, 1300, 290, 'cloud-platform', this.clouds);
+        var cloud3 = new CloudPlatform(this.game, 1300, 290, 'float1', this.clouds);
 
         cloud3.addMotionPath([
-            { x: "+450", xSpeed: 4000, xEase: "Expo.easeIn", y: "-200", ySpeed: 3000, yEase: "Linear" },
-            { x: "-500", xSpeed: 4000, xEase: "Expo.easeOut", y: "+200", ySpeed: 3000, yEase: "Linear" }
+            { x: "+470", xSpeed: 4000, xEase: "Linear", y: "-220", ySpeed: 3000, yEase: "Linear" },
+            { x: "-470", xSpeed: 4000, xEase: "Expo.easeOut", y: "+220", ySpeed: 3000, yEase: "Linear" }
         ]);
 
         //  The Player
-        this.player = this.add.sprite(24100, 0, 'dude');
+        this.player = this.add.sprite(2390, 0, 'egg');
 
         this.physics.arcade.enable(this.player);
 
@@ -127,7 +137,14 @@ PhaserGame.prototype = {
 
         this.clouds.callAll('start');
         fx = this.add.audio('burp');
+        death = this.add.audio('death');
+        jump = this.add.audio('jump');
+        goal = this.add.audio('goal');
         console.log(this.player.body);
+        text = this.add.text(10,10, "Find the slice of pizza", { font: "35px Arial", fill: "#ff0044", align: "center" });
+        
+
+        this.input.onDown.addOnce(removeText, this);
     },
 
     customSep: function (player, platform) {
@@ -210,19 +227,23 @@ PhaserGame.prototype = {
     update: function () {
 
         this.background.tilePosition.x = -(this.camera.x * 0.7);
-        this.trees.tilePosition.x = -(this.camera.x * 0.9);
+        this.mountain.tilePosition.x = -(this.camera.x * 0.9);
 
         this.physics.arcade.collide(this.player, this.stationary);
         this.physics.arcade.collide(this.player, this.clouds, this.customSep, null, this);
-        this.physics.arcade.overlap(this.player, this.diamond, hitDiamond, null, this);
+        this.physics.arcade.overlap(this.player, this.pizza, hitpizza, null, this);
         //  Do this AFTER the collide check, or we won't have blocked/touching set
         var standing = this.player.body.blocked.down || this.player.body.touching.down || this.locked;
 
         this.player.body.velocity.x = 0;
+        if (this.player.body.onFloor()){
+          goal.play();
+          this.player.kill();
+        }
 
         if (this.cursors.left.isDown)
         {
-            this.player.body.velocity.x = -150;
+            this.player.body.velocity.x = -220;
 
             if (this.facing !== 'left')
             {
@@ -232,7 +253,7 @@ PhaserGame.prototype = {
         }
         else if (this.cursors.right.isDown)
         {
-            this.player.body.velocity.x = 150;
+            this.player.body.velocity.x = 220;
 
             if (this.facing !== 'right')
             {
@@ -267,35 +288,48 @@ PhaserGame.prototype = {
             }
 
             this.willJump = true;
+            jump.play();
         }
 
         if (this.locked)
         {
             this.checkLock();
         }
-      function hitDiamond (player, diamond) {
+
+        // if(!this.player.inWorld){
+        //   this.player.kill();
+        // }
+      function hitpizza (player, pizza) {
         // Removes the player from the screen
         
-        diamond.kill();
-        // fx.play();
+        pizza.kill();
+        goal.play();
+        fx.play();
         // player.body.position.x=20;
         // player.body.position.y=0;  
-        // player.body.bounce.y=1;
+        player.body.bounce.y=1;
         player.body.angle=320;
 
 
         emitter.start(true, 2000, null, 10);
         var text = "Congrats!!!\n you found the pizza";
         var style = { font: "45px Arial", fill: "#ff0044", align: "center" };
-        var t = game.add.text(this.world.worldPosition, 100, text, style);
-
+        var t = game.add.text(1950,0, text, style);
+        // game.state.start('Game', PhaserGame, true); 
+        //player.reset(50,0);
+        game.time.events.add(2000, player.kill(), this);
+        
       }
-    }
+    },
 
 
 
 };
+function removeText() {
 
+    text.destroy();
+
+}
 CloudPlatform = function (game, x, y, key, group) {
 
     if (typeof group === 'undefined') { group = game.world; }
